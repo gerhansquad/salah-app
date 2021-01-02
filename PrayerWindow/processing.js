f8ddb79bad59faf9951f2695d7e4456a2b0571af
mma37 <mma37@hw.ac.uk>
2021-01-02 15:30:22 +0400

    Made the time left dynamic. Fixed #23

--- PrayerWindow/processing.js
+++ PrayerWindow/processing.js
@@ -1,8 +1,8 @@
-import { startUpdating } from "./view";
-import { makeTimestamp as timeStamp } from "./processing";
+import { startUpdating } from "./view"
+import { makeTimestamp as timeStamp } from "./processing"
 
 export function initPrayerView() {
-	let fname = "salah-times.json";
+	let fname = "salah-times.json"
 	window.requestFileSystem(
 		LocalFileSystem.PERSISTENT,
 		0,
@@ -10,81 +10,81 @@ export function initPrayerView() {
 			fs.root.getFile(fname, { create: false, exclusive: false }, function (fileEntry) {
 				fileEntry.file(
 					function (file) {
-						var reader = new FileReader();
+						var reader = new FileReader()
 						reader.onloadend = function () {
-							console.log("Successful file read: " + this.result);
-							let data = JSON.parse(this.result);
-							processData(data);
-						};
-						reader.readAsText(file);
+							console.log("Successful file read: " + this.result)
+							let data = JSON.parse(this.result)
+							processData(data)
+						}
+						reader.readAsText(file)
 					},
 					function (error) {
-						throw error;
+						throw error
 					}
-				);
-			});
+				)
+			})
 		},
 		function (error) {
-			throw error;
+			throw error
 		}
-	);
+	)
 }
 
 export function processData(data) {
-	let currentMonthPrayerData = {};
+	let currentMonthPrayerData = {}
 	for (var i = 0; i < data.data.length; i++) {
-		currentMonthPrayerData[`${i}`] = data.data[i].timings;
+		currentMonthPrayerData[`${i}`] = data.data[i].timings
 	}
 	// Getting today's day (1-31)
-	let currentDate = new Date();
-	let currentDay = currentDate.getDate();
+	let currentDate = new Date()
+	let currentDay = currentDate.getDate()
 
 	// Saving today's prayer data to a variable
-	let currentDayPrayerData = currentMonthPrayerData[`${currentDay}`]; // {"Fajr":"06:04 (PST)","Sunrise":"07:22 (PST)","Dhuhr":"12:10 (PST)",...}
+	let currentDayPrayerData = currentMonthPrayerData[`${currentDay}`] // {"Fajr":"06:04 (PST)","Sunrise":"07:22 (PST)","Dhuhr":"12:10 (PST)",...}
 
 	// Deleting Sunset as it is the same value as Maghrib
-	delete currentDayPrayerData["Sunset"];
+	delete currentDayPrayerData["Sunset"]
 
 	// Deleting Imsak as it didn't come sorted into the right position
-	delete currentDayPrayerData["Imsak"];
+	delete currentDayPrayerData["Imsak"]
 
 	/**
 	 * Pushing each Waqt's prayer time (already sorted) to an array so that
 	 * we can determine when in the timeline the system time falls
 	 */
-	let currentDayPrayerDataArray = [];
+	let currentDayPrayerDataArray = []
 	for (const waqt in currentDayPrayerData) {
-		currentDayPrayerDataArray.push(currentDayPrayerData[waqt]);
+		currentDayPrayerDataArray.push(currentDayPrayerData[waqt])
 	}
-	insertDateSorted(currentDayPrayerDataArray, currentDate);
-	startUpdating(currentDayPrayerData, currentDayPrayerDataArray);
+	insertDateSorted(currentDayPrayerDataArray, currentDate)
+	startUpdating(currentDate, currentDayPrayerData, currentDayPrayerDataArray)
 }
 
 // Inserts a date into a sorted list of dates in the right position
 function insertDateSorted(arr, key) {
 	try {
 		for (let i = 0; i < arr.length; i++) {
-			let time = arr[i].match("[0-9][0-9]:[0-9][0-9]")[0];
-			arr[i] = timeStamp(time);
+			let time = arr[i].match("[0-9][0-9]:[0-9][0-9]")[0]
+			arr[i] = timeStamp(time)
 		}
 
-		let i = arr.length - 1;
+		let i = arr.length - 1
 		while (i >= 0 && arr[i] > key) {
-			arr[i + 1] = arr[i];
-			i -= 1;
+			arr[i + 1] = arr[i]
+			i -= 1
 		}
-		arr[i + 1] = key;
+		arr[i + 1] = key
 	} catch (error) {
-		console.error(`insertDateSorted error: ${error}`);
+		console.error(`insertDateSorted error: ${error}`)
 	}
 }
 
 export function makeTimestamp(time) {
-	let date = new Date();
-	let hour = time.split(":")[0];
-	hour = hour == 0 ? 24 : hour;
-	date.setHours(hour);
-	date.setMinutes(time.split(":")[1]);
-	date.setSeconds(0);
-	return date;
+	let date = new Date()
+	let hour = time.split(":")[0]
+	hour = hour == 0 ? 24 : hour
+	date.setHours(hour)
+	date.setMinutes(time.split(":")[1])
+	date.setSeconds(0)
+	return date
 }
--- PrayerWindow/view.js
+++ PrayerWindow/view.js
@@ -1,39 +1,39 @@
-import { makeTimestamp } from "./processing";
+import { makeTimestamp } from "./processing"
 
-export function startUpdating(currentDayPrayerData, arr) {
-	let num = 0;
+export function startUpdating(currentTimestamp, currentDayPrayerData, arr) {
+	let num = 0
 	setInterval(function () {
-		let currentTimestamp = new Date();
-		let currentHour = currentTimestamp.getHours();
-		let currentMinute = currentTimestamp.getMinutes();
+		let timer = new Date()
+		let currentHour = timer.getHours()
+		let currentMinute = timer.getMinutes()
 
-		let prayerIndex = arr.indexOf(currentTimestamp);
+		let prayerIndex = arr.indexOf(currentTimestamp)
 
-		let currentPrayerIndex = prayerIndex == 0 ? arr.length - 1 : prayerIndex - 1;
-		let currentPrayerTime = arr[currentPrayerIndex];
+		let currentPrayerIndex = prayerIndex == 0 ? arr.length - 1 : prayerIndex - 1
+		let currentPrayerTime = arr[currentPrayerIndex]
 
-		let nextPrayerIndex = prayerIndex == arr.length - 1 ? 0 : prayerIndex + 1;
-		let nextPrayerTime = arr[nextPrayerIndex];
+		let nextPrayerIndex = prayerIndex == arr.length - 1 ? 0 : prayerIndex + 1
+		let nextPrayerTime = arr[nextPrayerIndex]
 
 		for (const waqt in currentDayPrayerData) {
 			// console.log(currentDayPrayerData[waqt]);
-			currentDayPrayerData[`${waqt}`] = makeTimestamp(String(currentDayPrayerData[waqt]).match("[0-9][0-9]:[0-9][0-9]")[0]);
+			currentDayPrayerData[`${waqt}`] = makeTimestamp(String(currentDayPrayerData[waqt]).match("[0-9][0-9]:[0-9][0-9]")[0])
 			// console.log(`2nd line: ${currentDayPrayerData[`${waqt}`]}`);
 			// console.log(`${waqt}: ${currentDayPrayerData[`${waqt}`]}`)
 		}
 
-		$("#first-prayer").text(getKeyByValue(currentDayPrayerData, currentPrayerTime));
-		$("#second-prayer").text(`left until ${getKeyByValue(currentDayPrayerData, nextPrayerTime)}`);
+		$("#first-prayer").text(getKeyByValue(currentDayPrayerData, currentPrayerTime))
+		$("#second-prayer").text(`left until ${getKeyByValue(currentDayPrayerData, nextPrayerTime)}`)
 
-		let hourInMs = nextPrayerTime.getHours() * 60 * 60 * 1000 - currentHour * 60 * 60 * 1000;
-		let minInMs = nextPrayerTime.getMinutes() * 60 * 1000 - currentMinute * 60 * 1000;
+		let hourInMs = nextPrayerTime.getHours() * 60 * 60 * 1000 - currentHour * 60 * 60 * 1000
+		let minInMs = nextPrayerTime.getMinutes() * 60 * 1000 - currentMinute * 60 * 1000
 
-		let hoursDifference = Math.floor(hourInMs / 1000 / 60 / 60);
-		let minutesDifference = Math.floor(minInMs / 1000 / 60);
-		$("#time-left").text(`${hoursDifference}h${minutesDifference}m`);
-	}, 1000);
+		let hoursDifference = Math.floor(hourInMs / 1000 / 60 / 60)
+		let minutesDifference = Math.floor(minInMs / 1000 / 60)
+		$("#time-left").text(`${hoursDifference}h${minutesDifference}m`)
+	}, 1000)
 
 	function getKeyByValue(object, value) {
-		return Object.keys(object).find((key) => object[key] == `${value}`);
+		return Object.keys(object).find((key) => object[key] == `${value}`)
 	}
 }
